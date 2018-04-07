fa0ec4e63fb7a1508bbd6a2754f6f261b17907c4
elhayr1 <elhay@robotican.net>
2018-04-07 19:37:13 +0300

    adding files

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # Strober
-Library for playing led light patterns in arduino
+
+Light your board led with predefined or custom patterns. Strober is a great tool for when you writing Arduino software, and want one of your board leds to operate as an indicator to all kind of situatuions, allowing you quicker debugging and getting info about the board current state.
+
+## Prerequisite
+Strober depends on SandTimer library to operate. SandTimer library can be found [here](https://github.com/elhayra/SandTimer). Download and copy it to Arduino libraries folder
+
+## Usage
+1. Include Strober.h in your code 
+2. Create new Strober instance. e.g.
+```
+Strober strober;
+```
+
+3. Set initial led notes. e.g.
+```
+void setup()
+{
+  ...
+  strober.setNotes(Strober::Notes::BLINK_SLOW);
+  ...
+}
+```
+
+4. Invoke play() method inside loop(), and set notes in run time according to your needs. e.g.
+```
+void loop()
+{
+  strober.play(INDICATOR_LED);
+  ...
+  ...
+  if (x)
+    strober.setNotes(Strober::Notes::STROBE);
+  else if (y)
+    strober.setNotes(Strober::Notes::BLINK_FAST);
+}
+```
+
+You can also invoke setNotes with a custom made notes array. e.g.
+```
+int16_t custom_notes[4] = {1, 500, 0, 500};
+strober.setNotes(custom_notes, 4);
+```
+Array size must be even, because each note is a pair i.e. [note, interval], so the array look like so:
+(note, interval, note, interval, ..., note, interval)
+note value can be 0 or 1, and interval is the time (in milliseconds) to wait before playing next note.
---
+++ Strober.h
@@ -0,0 +1,72 @@
+/*******************************************************************************
+* Copyright (c) 2018 Elhay Rauper
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted (subject to the limitations in the disclaimer
+* below) provided that the following conditions are met:
+*
+*     * Redistributions of source code must retain the above copyright notice,
+*     this list of conditions and the following disclaimer.
+*
+*     * Redistributions in binary form must reproduce the above copyright
+*     notice, this list of conditions and the following disclaimer in the
+*     documentation and/or other materials provided with the distribution.
+*
+*     * Neither the name of the copyright holder nor the names of its
+*     contributors may be used to endorse or promote products derived from this
+*     software without specific prior written permission.
+*
+* NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
+* THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+* PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+* IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+* POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#ifndef LED_H
+#define LED_H
+
+#include "SandTimer.h"
+
+class Strober
+{
+
+public:
+  enum Notes 
+  {
+    BLINK_FAST,
+    BLINK_SLOW,
+    STROBE
+  };
+  Strober(uint16_t pin);
+  void play();
+  void setNotes(uint16_t* notes, size_t notes_size);
+  void setNotes(Notes notes);
+
+  const static size_t SLOW_BLINK_SIZE = 4;
+  const static size_t FAST_BLINK_SIZE = 4;
+  const static size_t STROBE_BLINK_SIZE = 12;
+
+private:
+  SandTimer timer_;
+  int16_t pin_;
+  uint16_t* notes_;
+  size_t notes_size_ = 0;
+  uint16_t index_ = 0;
+  Notes curr_notes_;
+
+  const static uint16_t SLOW_BLINK_ARR[SLOW_BLINK_SIZE];
+  const static uint16_t FAST_BLINK_ARR[FAST_BLINK_SIZE];
+  const static uint16_t STROBE_BLINK_ARR[STROBE_BLINK_SIZE];
+
+  
+};
+#endif
